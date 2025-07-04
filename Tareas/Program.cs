96d0cbacb2d479f752aba81b4eb82aaddec7890e
Ruth <easerratic21@gmail.com>
2025-07-04 12:26:23 -0300

    hecho tarea

---
+++ Tareas/Program.cs
@@ -0,0 +1,36 @@
+﻿using EspacioTarea;
+using System.Text.Json;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+HttpClient client = new HttpClient();
+var url = "https://jsonplaceholder.typicode.com/todos/";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode(); // me da un status code
+
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Tarea>? PropsTarea = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+
+Console.WriteLine("\nTAREAS PENDIENTES");
+foreach (var prop in PropsTarea)
+{
+    if (!prop.Completed)
+    {
+        Console.WriteLine($"Titulo: {prop.Title} - Estado: {prop.Completed}");
+    }
+}
+
+Console.WriteLine("\nTAREAS REALIZADAS");
+foreach (var prop in PropsTarea)
+{
+    if (prop.Completed)
+    {
+        Console.WriteLine($"Titulo: {prop.Title} - Estado: {prop.Completed}");
+    }
+}
+string rutaReporte = Path.Combine(System.IO.Directory.GetCurrentDirectory(), "tareas.json");
+string jsonString = JsonSerializer.Serialize(PropsTarea);
+using (var writer = new StreamWriter(rutaReporte)) {
+    
+    writer.WriteLine(jsonString);
+}
---
+++ Tareas/Tarea.cs
@@ -0,0 +1,17 @@
+using System.Text.Json.Serialization;
+namespace EspacioTarea
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class Tarea
+    {
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+        [JsonPropertyName("completed")]
+        public bool Completed { get; set; }
+    }
+
+}
